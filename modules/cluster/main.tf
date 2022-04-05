a5b84a82e89a36c474ec4a64516e679cadd2e86c
SubroBlack <dahalsubodh8@gmail.com>
2022-04-05 09:58:39 +0300

    Public Cluster

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -20,13 +20,13 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-
+/*
   private_cluster_config {
     enable_private_nodes = true
     enable_private_endpoint = false
     master_ipv4_cidr_block = "10.5.6.0/28"
   }
-
+ */
   remove_default_node_pool = true
   initial_node_count       = 1
 
